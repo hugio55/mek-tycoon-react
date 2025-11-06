80b1a50dee7e32ad0cf49e4c6a1e6e231f9c2cb9
Ben Meyers <ben@mektycoon.com>
2025-11-05 23:06:01 -0500

    Auto-save: Wed 11/05/2025 23:06:01.19

--- src/components/MekProfileLightbox.tsx
+++ src/components/MekProfileLightbox.tsx
@@ -1679,8 +1679,8 @@ function VariationCard({
               className="absolute inset-0 pointer-events-none"
               style={{
                 opacity: noiseIntensity,
-                mixBlendMode: 'multiply',
-                backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+                mixBlendMode: 'overlay',
+                backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3CfeColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 -0.5'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' fill='%23000000'/%3E%3C/svg%3E")`,
                 backgroundSize: 'cover'
               }}
             />
