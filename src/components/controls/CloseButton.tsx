c8dc48f2ef2c6049cdb102176ed064894160b7bd
Ben Meyers <ben@mektycoon.com>
2025-11-05 21:08:58 -0500

    Auto-save: Wed 11/05/2025 21:08:58.19

---
+++ src/components/controls/CloseButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import './CloseButton.css';
+
+interface CloseButtonProps {
+  onClick?: () => void;
+  className?: string;
+}
+
+export default function CloseButton({ onClick, className = '' }: CloseButtonProps) {
+  return (
+    <div
+      className={`close-container ${className}`}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      aria-label="Close"
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          onClick?.();
+        }
+      }}
+    >
+      <div className="leftright"></div>
+      <div className="rightleft"></div>
+      <label className="close">close</label>
+    </div>
+  );
+}
