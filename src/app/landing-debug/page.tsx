ca2062450950636d46d9d2826fa78456927d8b2a
Ben Meyers <ben@mektycoon.com>
2025-11-10 15:36:06 -0500

    Auto-save: Mon 11/10/2025 15:36:06.18

--- src/app/landing-debug/page.tsx
+++ src/app/landing-debug/page.tsx
@@ -384,7 +384,6 @@ export default function LandingDebugPage() {
             ))}
           </div>
         </div>
-      </div>
 
         {/* Debug Controls Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
