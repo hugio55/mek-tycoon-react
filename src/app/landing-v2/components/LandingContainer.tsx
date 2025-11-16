90d4f3e12e219b3084bf55367ff3fc2fb750ca17
Ben Meyers <ben@mektycoon.com>
2025-11-16 13:36:36 -0500

    Auto-save: Sun 11/16/2025 13:36:36.24

--- src/app/landing-v2/components/LandingContainer.tsx
+++ src/app/landing-v2/components/LandingContainer.tsx
@@ -18,7 +18,7 @@ export default function LandingContainer({ children, backgroundOpacity, showFoot
   }, []);
 
   return (
-    <div className={`h-screen bg-black flex flex-col landing-scrollbar ${allowScroll ? 'overflow-y-auto' : 'overflow-hidden'}`}>
+    <div className={`min-h-screen bg-black flex flex-col landing-scrollbar ${allowScroll ? 'overflow-y-auto' : 'overflow-hidden'}`}>
       <div
         className="fixed inset-0 transition-opacity ease-out pointer-events-none"
         style={{
