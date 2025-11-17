fff551abf62b104fb12df96840bf0228c6544db7
Ben Meyers <ben@mektycoon.com>
2025-11-17 15:30:28 -0500

    Auto-save: Mon 11/17/2025 15:30:28.27

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,16 +1,5 @@
-// Sentry instrumentation file for Next.js 15+
-// This file is required when using @sentry/nextjs with App Router
-
-import * as Sentry from '@sentry/nextjs';
+// Instrumentation disabled - Sentry removed from project
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
-  }
+  // No instrumentation needed
 }
-
-export const onRequestError = Sentry.captureRequestError;
