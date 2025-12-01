3cdd922611e6d593bdf30198075553314f01f66f
Ben Meyers <ben@mektycoon.com>
2025-12-01 15:53:36 -0500

    Auto-save: Mon 12/01/2025 15:53:36.24

--- src/instrumentation-client.ts
+++
@@ -1,3 +0,0 @@
-// Client-side instrumentation for Next.js
-// This file is required by Next.js 15+ when using instrumentation
-export {};
--- src/instrumentation.ts
+++
@@ -1,5 +0,0 @@
-// Server-side instrumentation for Next.js
-// This file is required by Next.js 15+ when using instrumentation
-export async function register() {
-  // Optional: Add server-side instrumentation here
-}
